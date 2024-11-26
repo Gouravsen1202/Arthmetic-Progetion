25bb7c7aaae98daa19678d20196bb912dd9227fa
Gouravsen1202 <gouravsen757@gmail.com>
2024-11-26 16:40:45 +0530

    first commit

---
+++ A P PROGRAM.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=1;
+	int d=2;
+	int n=5;
+	int term;
+	for(int i=0;i<n;i++)
+	{
+		term=a+i*d;
+	cout<<term<<endl;
+	}
+}
+//a+(n-i)d == matlab a p serice leke bnsns hai 
